86058f10b0e56d02c93e260d273c862743607b2f
jean-christopheSANCHEZ <jc@PC-JC.localdomain>
2021-10-26 19:02:36 +0200

    tp1 presque bon init gdt

--- tp1/tp.c
+++ tp1/tp.c
@@ -5,7 +5,13 @@
 extern info_t *info;
 
 
-void print_gdtr(gdt_reg_t my_gdtr){
+/*init my own gdt*/
+gdt_reg_t new_gdtr;
+seg_desc_t gdtSegments[4];
+
+void print_gdtr(){
+    gdt_reg_t my_gdtr;
+    get_gdtr(my_gdtr);
     int nb_segment = 0;
     nb_segment = (my_gdtr.limit +1 ) / sizeof(seg_desc_t);
     printf("\n\n                     =========GDTR=========n\n");
@@ -29,16 +35,99 @@ void print_gdtr(gdt_reg_t my_gdtr){
     }
 }
 
-void init_gdt(gdt_reg_t * gdt){
+void fill_gdt(seg_desc_t * gdtSegments, uint32_t limit, uint32_t base, uint64_t type, uint64_t s, uint64_t dpl){
+    gdtSegments->raw = 0ULL;
     
+    //set the limit bits of the seg
+    gdtSegments->limit_1 = limit & 0xFFFF; //on prend uniquement les 16 bits
+    gdtSegments->limit_2 = (limit >> 16) & 0xF; //on decale de 16bits puis on prend 4 bits
+
+    //set the base bits of the seg
+    gdtSegments->base_1 = base & 0xFFFF;
+    gdtSegments->base_2 = (base >> 16) & 0xff;
+    gdtSegments->base_3 = (base >> 24) & 0xff;
+
+    //set the type
+    gdtSegments->type = type;
+
+    // set s : descriptor type
+    gdtSegments->s = s;
+
+    //set dpl : descriptor privilege level
+    gdtSegments->dpl = dpl;
+
+    //set p : segment present flag : 1
+    gdtSegments->p = 1;
+
+    //set l : longmode : 0 because 32bits
+    gdtSegments->l = 0;
+
+    //set d : default lenght : 1 because we are 32bits
+    gdtSegments->d = 1;
+
+    //set granularity
+    gdtSegments->g = 1;
+
+    //set avl : 0
+    gdtSegments->avl = 0;
+
+    gdtSegments->raw = 0ULL;
+}   
+
+
+void init_gdt(){
+        //init the first segment 0 indice : NULL value
+    gdtSegments[0].raw = 0ULL;
+
+    // init the second segment : 1 indice for code
+    fill_gdt(&gdtSegments[1],
+        /*limit addr max on 32 bits : 0xFFFFFF*/0xFFFFFF,
+         /*base addr : 0*/0 ,
+         /*type : rx*/ SEG_DESC_CODE_XR,
+         /*s because type code : 1 */1,
+         /*dpl : ring0 : 0*/ 0
+    );
+
+    // init the third segment : 2 indice for data
+    fill_gdt(&gdtSegments[2],
+        /*limit addr max on 32 bits : 0xFFFFFF*/0xFFFFFF,
+         /*base addr : 0*/0 ,
+         /*type : rx*/ SEG_DESC_DATA_RW,
+         /*s because type data : 1 */1,
+         /*dpl : ring0 : 0*/ 0
+    );
+
+    //init the last segment at NULL
+    gdtSegments[3].raw = 0ULL;
+
+
+    //init gdt : 
+    //gdtr limit :
+    new_gdtr.limit = sizeof(gdtSegments) - 1;
+    //init the segments
+    new_gdtr.desc = gdtSegments;
+
+    //load my new gdtr
+    set_gdtr(new_gdtr);
+
+    //load segments selectors
+    set_cs(gdt_krn_seg_sel(1));
+
+    set_ds(gdt_krn_seg_sel(2));
+    set_es(gdt_krn_seg_sel(2));
+    set_ss(gdt_krn_seg_sel(2));
+    set_fs(gdt_krn_seg_sel(2));
+    set_gs(gdt_krn_seg_sel(2));
+
 }
 
 void tp(){
-    gdt_reg_t my_gdtr;
-    get_gdtr(my_gdtr);
+    
+    //print this gdt
+    print_gdtr();   
 
-    print_gdtr(my_gdtr);
-
+    // init my own gdt
     init_gdt();
+    print_gdtr();
     
 }
